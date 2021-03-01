404a07b24d78462e19ef90434c771d1a8d0faecf
eugeneswee <eugeneswee@yahoo.com>
2021-03-01 13:24:39 +0000

    done

--- app/app.py
+++ app/app.py
@@ -7,6 +7,6 @@ def home():
         return render_template('index.html', bg_color=bg_color, font_color=font_color)
 
 if __name__ == '__main__':
-        app.run(port=5000, host="0.0.0.0")
+        app.run(port=8080, host="0.0.0.0")
     #margin-top: -100px; 
     #margin-left: -900px; 
