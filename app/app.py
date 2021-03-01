a8f9b1634a7900ba808df00b92f7f097dcc7ce03
TanChongYong <36144775+TanChongYong@users.noreply.github.com>
2021-03-01 17:45:17 +0800

    Add files via upload

--- app/app.py
+++ app/app.py
@@ -1,8 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 @app.route("/")
 def home():
-    	    return "Hello, Flask!"
+        bg_color = "black"
+        font_color = "white"
+        return render_template('index.html', bg_color=bg_color, font_color=font_color)
 
 if __name__ == '__main__':
         app.run(port=5000, host="0.0.0.0")
