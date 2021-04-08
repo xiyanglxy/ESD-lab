7955d4442ddfcfb1596b8f4662818591722bb359
xiyang <xiyanglxy@gmail.com>
2021-04-08 11:46:36 +0800

    changed bgcolor to blue

--- app/app.py
+++ app/app.py
@@ -1,12 +1,15 @@
 from flask import Flask, render_template
 app = Flask(__name__)
+
+
 @app.route("/")
 def home():
-        bg_color = "black"
-        font_color = "white"
-        return render_template('index.html', bg_color=bg_color, font_color=font_color)
+    bg_color = "blue"
+    font_color = "white"
+    return render_template('index.html', bg_color=bg_color, font_color=font_color)
+
 
 if __name__ == '__main__':
-        app.run(port=5000, host="0.0.0.0")
-    #margin-top: -100px; 
-    #margin-left: -900px; 
+    app.run(port=8080, host="0.0.0.0")
+# margin-top: -100px;
+# margin-left: -900px;
